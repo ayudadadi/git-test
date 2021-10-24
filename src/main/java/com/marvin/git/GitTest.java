1bae4e52415de771898788cbcd2338d102853cec
Marvin <ayuisnice@outlook.com>
2021-10-24 21:56:30 +0800

    first commit

---
+++ src/main/java/com/marvin/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.marvin.git;
+
+/**
+ * @author Marvin
+ * @Description com.marvin.git
+ * @create 2021-10-24 21:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,Git");
+    }
+}
